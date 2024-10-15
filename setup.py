08e1505559d13e87821534eb3383c7717c634020
UĞUR ORTAÇ <ugurortac@windowslive.com>
2024-10-16 00:13:03 +0300

    Update Setup File

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
 from setuptools import setup, find_packages
 
-# requirements.txt dosyasını okuyup bağımlılıkları alır
+# Reads the requirements.txt file and extracts dependencies
 def parse_requirements(filename):
     with open(filename, 'r') as f:
         lines = f.readlines()
-    # Yorum satırlarını ve boş satırları filtreler
+    # Filters out comment lines and empty lines
     requirements = [line.strip() for line in lines if line.strip() and not line.startswith('#')]
     return requirements
 
 setup(
-    name='socialMediaNew',  # Paketinizin adı
-    version='1.0.0',  # Sürüm numarası
-    author='Uğur Ortaç',  # Yazar adı
-    author_email='info@ugurortac.com',  # Yazarın e-posta adresi
-    description='Social Media Analysis with GPU Support',  # Kısa açıklama
-    long_description=open('README.md').read(),  # Uzun açıklama (README dosyası)
-    long_description_content_type='text/markdown',  # README dosyasının formatı
-    url='https://github.com/yourusername/socialMediaNew',  # Projenizin URL'si
-    packages=find_packages(where='src'),  # Paketlerin bulunduğu dizin
-    package_dir={'': 'src'},  # Paketlerin kök dizini
-    install_requires=parse_requirements('requirements.txt'),  # Bağımlılıklar
+    name='social-media-opinion-analysis',  # Name of the package
+    version='1.0.0',  # Version number
+    author='Uğur Ortaç',  # Author's name
+    author_email='info@ugurortac.com',  # Author's email address
+    description='Social Media Opinion Analysis with GPU Support',  # Short description
+    long_description=open('README.md').read(),  # Long description (README file)
+    long_description_content_type='text/markdown',  # Format of the README file
+    url='https://github.com/H4ck3rZ0n3/social-media-opinion-analysis',  # Project URL
+    packages=find_packages(where='src'),  # Directory where packages are located
+    package_dir={'': 'src'},  # Root directory of the packages
+    install_requires=parse_requirements('requirements.txt'),  # Dependencies
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',  # Lisans türü
+        'License :: OSI Approved :: GNU License 3.0',  # Type of license
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.10',  # Python sürüm gereksinimi
+    python_requires='>=3.10',  # Minimum required Python version
     entry_points={
         'console_scripts': [
-            'socialMediaNew=main:main',  # Komut satırı komutu (isteğe bağlı)
+            'social-media-opinion-analysis=main:main',  # Command-line command (optional)
         ],
     },
 )
